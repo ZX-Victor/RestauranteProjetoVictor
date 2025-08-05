c6204640a9a1dab71fc539433ac4cf104e613f14
cj3027414 <cj3027414@LAB02-PC20>
2025-08-05 13:00:12 -0300

    Adicionar arquivos de projeto.

---
+++ RestauranteProjetoVictor/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace RestauranteProjetoVictor
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSignup = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.lblUser1 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSignup
+            // 
+            this.lblSignup.AutoSize = true;
+            this.lblSignup.BackColor = System.Drawing.Color.White;
+            this.lblSignup.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSignup.Location = new System.Drawing.Point(257, 137);
+            this.lblSignup.Name = "lblSignup";
+            this.lblSignup.Size = new System.Drawing.Size(188, 24);
+            this.lblSignup.TabIndex = 0;
+            this.lblSignup.Text = "Faça o seu Cadastro:";
+            this.lblSignup.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(301, 214);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.BackColor = System.Drawing.Color.White;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(239, 218);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(49, 16);
+            this.lblPassword.TabIndex = 4;
+            this.lblPassword.Text = "Senha:";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(301, 174);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(100, 20);
+            this.txtUser.TabIndex = 5;
+            this.txtUser.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            // 
+            // lblUser1
+            // 
+            this.lblUser1.AutoSize = true;
+            this.lblUser1.BackColor = System.Drawing.Color.White;
+            this.lblUser1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUser1.Location = new System.Drawing.Point(231, 174);
+            this.lblUser1.Name = "lblUser1";
+            this.lblUser1.Size = new System.Drawing.Size(57, 16);
+            this.lblUser1.TabIndex = 6;
+            this.lblUser1.Text = "Usuário:";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(407, 192);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(90, 23);
+            this.btnLogin.TabIndex = 7;
+            this.btnLogin.Text = "Fazer Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lblUser1);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblSignup);
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login do Funcionario";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSignup;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label lblUser1;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
---
+++ RestauranteProjetoVictor/Form2.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace RestauranteProjetoVictor
+{
+    partial class frmFood
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDrinks1 = new System.Windows.Forms.Button();
+            this.btnSweets1 = new System.Windows.Forms.Button();
+            this.btnChoose = new System.Windows.Forms.Button();
+            this.lblSnack = new System.Windows.Forms.Label();
+            this.lblSweets = new System.Windows.Forms.Label();
+            this.lblDrinks = new System.Windows.Forms.Label();
+            this.txtSnack1 = new System.Windows.Forms.TextBox();
+            this.txtSweets1 = new System.Windows.Forms.TextBox();
+            this.txtDrinks1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnDrinks1
+            // 
+            this.btnDrinks1.Location = new System.Drawing.Point(470, 353);
+            this.btnDrinks1.Name = "btnDrinks1";
+            this.btnDrinks1.Size = new System.Drawing.Size(108, 27);
+            this.btnDrinks1.TabIndex = 0;
+            this.btnDrinks1.Text = "Escolha a bebida";
+            this.btnDrinks1.UseVisualStyleBackColor = true;
+            // 
+            // btnSweets1
+            // 
+            this.btnSweets1.Location = new System.Drawing.Point(470, 200);
+            this.btnSweets1.Name = "btnSweets1";
+            this.btnSweets1.Size = new System.Drawing.Size(108, 27);
+            this.btnSweets1.TabIndex = 1;
+            this.btnSweets1.Text = "Escolha o doce";
+            this.btnSweets1.UseVisualStyleBackColor = true;
+            // 
+            // btnChoose
+            // 
+            this.btnChoose.Location = new System.Drawing.Point(470, 47);
+            this.btnChoose.Name = "btnChoose";
+            this.btnChoose.Size = new System.Drawing.Size(108, 25);
+            this.btnChoose.TabIndex = 2;
+            this.btnChoose.Text = "Escolha o salgado";
+            this.btnChoose.UseVisualStyleBackColor = true;
+            // 
+            // lblSnack
+            // 
+            this.lblSnack.AutoSize = true;
+            this.lblSnack.BackColor = System.Drawing.Color.White;
+            this.lblSnack.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSnack.Location = new System.Drawing.Point(333, 32);
+            this.lblSnack.Name = "lblSnack";
+            this.lblSnack.Size = new System.Drawing.Size(69, 16);
+            this.lblSnack.TabIndex = 3;
+            this.lblSnack.Text = "Salgados:";
+            this.lblSnack.Click += new System.EventHandler(this.lblSnack_Click);
+            // 
+            // lblSweets
+            // 
+            this.lblSweets.AutoSize = true;
+            this.lblSweets.BackColor = System.Drawing.Color.White;
+            this.lblSweets.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSweets.Location = new System.Drawing.Point(333, 190);
+            this.lblSweets.Name = "lblSweets";
+            this.lblSweets.Size = new System.Drawing.Size(50, 16);
+            this.lblSweets.TabIndex = 4;
+            this.lblSweets.Text = "Doces:";
+            // 
+            // lblDrinks
+            // 
+            this.lblDrinks.AutoSize = true;
+            this.lblDrinks.BackColor = System.Drawing.Color.White;
+            this.lblDrinks.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDrinks.Location = new System.Drawing.Point(333, 331);
+            this.lblDrinks.Name = "lblDrinks";
+            this.lblDrinks.Size = new System.Drawing.Size(61, 16);
+            this.lblDrinks.TabIndex = 5;
+            this.lblDrinks.Text = "Bebidas:";
+            // 
+            // txtSnack1
+            // 
+            this.txtSnack1.Location = new System.Drawing.Point(336, 52);
+            this.txtSnack1.Name = "txtSnack1";
+            this.txtSnack1.Size = new System.Drawing.Size(120, 20);
+            this.txtSnack1.TabIndex = 6;
+            // 
+            // txtSweets1
+            // 
+            this.txtSweets1.Location = new System.Drawing.Point(336, 209);
+            this.txtSweets1.Name = "txtSweets1";
+            this.txtSweets1.Size = new System.Drawing.Size(120, 20);
+            this.txtSweets1.TabIndex = 7;
+            // 
+            // txtDrinks1
+            // 
+            this.txtDrinks1.Location = new System.Drawing.Point(336, 360);
+            this.txtDrinks1.Name = "txtDrinks1";
+            this.txtDrinks1.Size = new System.Drawing.Size(120, 20);
+            this.txtDrinks1.TabIndex = 8;
+            // 
+            // frmFood
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtDrinks1);
+            this.Controls.Add(this.txtSweets1);
+            this.Controls.Add(this.txtSnack1);
+            this.Controls.Add(this.lblDrinks);
+            this.Controls.Add(this.lblSweets);
+            this.Controls.Add(this.lblSnack);
+            this.Controls.Add(this.btnChoose);
+            this.Controls.Add(this.btnSweets1);
+            this.Controls.Add(this.btnDrinks1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "frmFood";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDrinks1;
+        private System.Windows.Forms.Button btnSweets1;
+        private System.Windows.Forms.Button btnChoose;
+        private System.Windows.Forms.Label lblSnack;
+        private System.Windows.Forms.Label lblSweets;
+        private System.Windows.Forms.Label lblDrinks;
+        private System.Windows.Forms.TextBox txtSnack1;
+        private System.Windows.Forms.TextBox txtSweets1;
+        private System.Windows.Forms.TextBox txtDrinks1;
+    }
+}
